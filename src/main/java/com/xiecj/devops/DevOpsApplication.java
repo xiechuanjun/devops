8a821d0353f312270458139d4af001b4b3fde686
xiechuanj <xiechuanj@gmail.com>
2020-09-24 15:52:12 +0800

    增加空行，进行演示

--- src/main/java/com/xiecj/devops/DevOpsApplication.java
+++ src/main/java/com/xiecj/devops/DevOpsApplication.java
@@ -3,6 +3,8 @@ package com.xiecj.devops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class DevOpsApplication {
     public static void main(String[] args){
